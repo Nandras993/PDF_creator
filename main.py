d0bed7f138e677c1f8c88991e0a62ef8d352415d
Nandras993 <n.andras993@gmail.com>
2022-12-28 12:53:01 +0100

    Add footers

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fpdf import FPDF
 import pandas as pd
 
-
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
@@ -14,7 +14,19 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(x1=10, y1=22, x2=200, y2=22)
 
+    # Set the footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+
+        # Set the footer
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 pdf.output("output.pdf")
